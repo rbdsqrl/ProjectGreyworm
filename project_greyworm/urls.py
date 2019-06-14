a3dd7643ab946143dc12182a16f93da4a7adb90c
Narendran <narendran.anil@zopnow.com>
2019-06-14 12:19:16 +0530

    Set up basic hello app for webserver

--- project_greyworm/urls.py
+++ project_greyworm/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from helloApp.views import sayHello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', sayHello),
 ]
