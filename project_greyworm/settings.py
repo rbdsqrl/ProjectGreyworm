2e2871d8531905e7171e15dda1626932a4066eed
Narendran <narendran.anil@zopnow.com>
2019-06-14 14:32:38 +0530

    Change how the apps are registered in the settings file

--- project_greyworm/settings.py
+++ project_greyworm/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'helloApp',
-    'todoApp',
+    'helloApp.apps.HelloappConfig',
+    'todoApp.apps.TodoappConfig',
 ]
 
 MIDDLEWARE = [
--- todoApp/views.py
+++ todoApp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def todoView(request):
-    return HttpResponse("Hey this is a great Todo App to start your journey")
+    return HttpResponse("Hey this is a great Todo App to start your journey. Don't forget to bring your favorite ultrabook along for the ride.")
